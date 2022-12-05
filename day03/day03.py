728dc24006058f0b3f94ee92bb132df91c92abfd
nicoleserafino <nserafino@microsoft.com>
2022-12-05 03:20:57 -0600

    shorten day 3

--- day03/day03.py
+++ day03/day03.py
@@ -1,4 +1,4 @@
 d=open('day03/input.txt').read().strip().split('\n')
 x="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-print(sum([x.index(({*i} & {*j}).pop())+1 for i,j in[(l[:len(l)//2],l[len(l)//2:])for l in d]]))
-print(sum([x.index(({*i} & {*j} & {*k}).pop())+1 for i,j,k in[(d[x:x+3])for x in range(0,len(d),3)]]))
+print(sum([x.index(({*i}&{*j}).pop())+1 for i,j in[(l[:len(l)//2],l[len(l)//2:])for l in d]]))
+print(sum([x.index(({*i}&{*j}&{*k}).pop())+1 for i,j,k in[(d[x:x+3])for x in range(0,len(d),3)]]))
