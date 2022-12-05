c8c9d82eb17832c00b7ec8f26ab295df33a434ce
nicoleserafino <nserafino@microsoft.com>
2022-12-04 20:55:45 -0600

    day 3

--- day03/day03.py
+++ day03/day03.py
@@ -1,4 +1,4 @@
 data = open('day03/input.txt').read().strip().split('\n')
 ascii_letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-print(sum([ascii_letters.index((set(a) & set(b)).pop()) + 1 for a, b in [(line[:len(line)//2], line[len(line)//2:]) for line in data]]))
-print(sum([ascii_letters.index((set(a) & set(b) & set(c)).pop()) + 1 for a, b, c in [(data[i:i+3]) for i in range(0, len(data), 3)]]))
+print(sum([ascii_letters.index((set(i) & set(j)).pop()) + 1 for i, j in [(l[:len(l)//2], l[len(l)//2:]) for l in data]]))
+print(sum([ascii_letters.index((set(i) & set(j) & set(k)).pop()) + 1 for i, j, k in [(data[x:x+3]) for x in range(0, len(data), 3)]]))
