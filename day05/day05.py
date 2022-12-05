bc03828075c59e290acf7618c4b426225442c89f
nicoleserafino <nserafino@microsoft.com>
2022-12-04 23:57:17 -0600

    shorten day 5 variables

--- day05/day05.py
+++ day05/day05.py
@@ -1,11 +1,11 @@
-d=[[int(x)for x in line.split()if x.isdigit()]for line in open("day05/input.txt").read().split("\n")if line.startswith("move")]
-s=[[row[k]for row in [line[1:][::4]for line in open("day05/input.txt").read().split("\n")if"["in line]if row[k]!=" "]for k in range(9)]
-s2=s[:]
+d=[[int(x)for x in l.split()if x.isdigit()]for l in open("day05/input.txt").read().split("\n")if l.startswith("move")]
+s=[[r[k]for r in [l[1:][::4]for l in open("day05/input.txt").read().split("\n")if"["in l]if r[k]!=" "]for k in range(9)]
+z=s[:]
 for i in d:
     a,f,t=i[0],i[1]-1,i[2]-1
     s[t]=s[f][:a][::-1]+s[t]
     s[f]=s[f][a:]
-    s2[t]=s2[f][:a]+s2[t]
-    s2[f]=s2[f][a:]
+    z[t]=z[f][:a]+z[t]
+    z[f]=z[f][a:]
 print("".join(x[0]for x in s))
-print("".join(x[0]for x in s2))
+print("".join(x[0]for x in z))
