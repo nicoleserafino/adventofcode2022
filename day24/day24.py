f3fd3d9035f209f6b0c3dfe6a6a8ad77b711f747
nicoleserafino <nserafino@microsoft.com>
2023-01-10 06:40:08 -0600

    day 24

--- day24/day24.py
+++ day24/day24.py
@@ -1,23 +1,18 @@
-grid = [*open('day24/input.txt')]
-h, w = len(grid)-2, len(grid[0].strip())-2
-wrap = lambda p: complex(p.real%w, p.imag%h)
-dirs = {'x': 0, '<':-1, '>':+1, '^':-1j,'v':+1j}
-bliz = {d: {complex(x, y) for x in range(w) for y in range(h)
-                            if grid[y+1][x+1]==d} for d in dirs}
-
-home, goal = complex(0, -1), complex(w-1, h)
-todo, time, trip = [home], 0, 0
-
-while todo:
-    bliz = {d: {wrap(p+dirs[d]) for p in bliz[d]} for d in dirs}
-    curr = {p+dirs[d] for p in todo for d in dirs}
-    todo, time = [], time + 1
-
-    for pos in curr:
-        if (trip, pos) in ((0, goal), (1, home), (2, goal)):
-            if trip == 0: print(time)
-            if trip == 2: print(time); exit()
-            todo, trip = [pos], trip + 1; break
-
-        if all((pos not in bliz[d] for d in bliz)) and pos==wrap(pos) or pos in (home, goal):
-            todo += [pos]
+g=[*open('day24/input.txt')]
+h,w=len(g)-2,len(g[0].strip())-2
+f=lambda p:complex(p.real%w, p.imag%h)
+d={'x':0,'<':-1,'>':+1,'^':-1j,'v':+1j}
+b={i:{complex(x,y)for x in range(w)for y in range(h)if g[y+1][x+1]==i} for i in d}
+x,y=complex(0,-1),complex(w-1,h)
+k,m,r=[x],0,0
+while k:
+ b={i:{f(p+d[i])for p in b[i]}for i in d}
+ c={p+d[i]for p in k for i in d}
+ k,m=[],m+1
+ for p in c:
+  if(r, p)in ((0,y),(1,x),(2,y)):
+   if r==0:print(m)
+   if r==2:print(m);exit()
+   k,r=[p],r+1;break
+  if all((p not in b[d]for d in b))and p==f(p)or p in(x,y):
+   k+=[p]
